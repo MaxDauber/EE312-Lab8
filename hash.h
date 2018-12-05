72a87cc72a314d9690e19ea4df313a4a87c40471
jacobgrimm <36799356+jacobgrimm@users.noreply.github.com>
2018-12-04 22:32:36 -0600

    hash.h
    the hash table header file has been created

---
+++ hash.h
@@ -0,0 +1,32 @@
+#ifndef _HASH_H
+#define _HASH_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+class Hash
+{
+private:
+    static const int TABLE_SIZE = 512;
+    struct HashNode
+    {
+        int data;
+        HashNode *next;
+
+    };
+
+    HashNode * Table [TABLE_SIZE];
+
+public:
+    void hash (string &s);
+    int * getcollisions(void);
+
+};
+
+
+#endif
+
+
