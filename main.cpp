176904920ee3047d1f08e42e1973d85a8834ba37
jacobgrimm <36799356+jacobgrimm@users.noreply.github.com>
2018-12-04 21:34:28 -0600

    Stage1
    stAGE1

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <queue>
+
 
 using namespace std;
+
 
 /*function... might want it in some class?*/
 int getdir (string dir, vector<string> &files)
@@ -23,16 +27,71 @@ int getdir (string dir, vector<string> &files)
     closedir(dp);
     return 0;
 }
-
-int main()
-{
-    string dir = string("sm_doc_set");
-    vector<string> files = vector<string>();
-
-    getdir(dir,files);
-
-    for (unsigned int i = 0;i < files.size();i++) {
-        cout << i << files[i] << endl;
+void cleaner (string& helo) {//function created to clean up strings and make them all caps
+    for (int i = 0; i < helo.length(); i++) {
+        if (96 < helo[i] && helo[i] < 123) {
+            helo[i] = helo[i] - 32;
+        }
+        if(helo[i]<64 || helo[i]>91){
+            helo.erase(helo.begin()+i);
+            i--;
+        }
     }
-    return 0;
 }
+
+int main(int argc, char* argv[]) {
+        long long count =0;
+        //string dir = argv[1];//string("sm_doc_set");
+        string dir = string("sm_doc_set");
+        vector<string> files = vector<string>();
+
+        getdir(dir, files);
+        for (unsigned int i = 0; i < files.size(); i++) {
+            cout << i << files[i] << endl;
+        }
+        string Mystring;
+        ifstream myfile;
+        for (int i = 2; i < files.size(); i++) {
+            string fileptr = "sm_doc_set/" + files[i];
+//            string fileptr = dir + '/ + files[i];
+
+            myfile.open(fileptr.c_str());
+            if (myfile.good() && myfile.is_open()) {
+                cout << "good job fam";
+                string mess;
+               // string mess2;
+                vector<string> less;
+                //for (int i = 0; i < 6; i++) {
+                   while(!myfile.eof()) {
+                       while (less.size()<=6 && !myfile.eof())// replace 6 with the n word sequence
+                       {
+                           myfile >> mess;
+                           cleaner(mess);
+                           if (mess.length() != 0) {
+                               less.push_back(mess);
+                           }
+                       }
+                       for(int j=0; j<less.size();j++){
+                           cout<<less[j]<<" ";
+                       }
+                       less.erase(less.begin());
+                       count++;
+                       cout<< endl;
+                   }
+                //cleaner(mess2);
+                //cout << mess2;
+               /* while(!less.empty()){
+                    cout<<*(less.begin())<<" ";
+                    less.erase(less.begin());
+                } */
+
+            } else {
+                cout << "usuc";
+                return 0;
+            }
+            cout << endl;
+            myfile.close();
+        }
+        cout<<"count is "<<count;
+}
+
